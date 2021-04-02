64d46964948c6ed851b02835a2b4785b32d96b85
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-04-02 12:39:41 +0530

    chore: render note from data

--- index.js
+++ index.js
@@ -6,13 +6,41 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const notes = [
+  {
+    id: 1,
+    title: 'I wrote this note today',
+    createdAt: '6:07 AM'
+  },
+  {
+    id: 2,
+    title: 'I wrote this note today',
+    createdAt: '6:07 AM'
+  },
+  {
+    id: 3,
+    title: 'I wrote this note today',
+    createdAt: '6:07 AM'
+  },
+  {
+    id: 1,
+    title: 'I wrote this note today',
+    createdAt: '6:07 AM'
+  },
+  {
+    id: 5,
+    title: 'I wrote this note today',
+    createdAt: '6:07 AM'
+  },
+]
+
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { notes });
 });
 
 
